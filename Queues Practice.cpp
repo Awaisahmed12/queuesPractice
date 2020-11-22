f72717808fd5e411cba6d4e1d5390054c73fa4aa
Awais Ahmed <ahmedawais672@gmail.com>
2020-11-22 16:10:18 -0600

    Add files via upload

---
+++ Queues Practice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+struct Node
+{
+    int value;
+    Node* next;
+    
+};
+class Queue
+{
+    int count;
+    
+};
+
+
+
+
+
+int main()
+{
+
+
+return 0;
+}
